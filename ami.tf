9afcb113cdb9b0b16b95a31b4baf41abd2bec257
kihwan <emotion1998k@gmail.com>
2024-06-05 11:51:23 +0900

    ami added

---
+++ ami.tf
@@ -0,0 +1,26 @@
+# Data source for Amazon Linux 2 Amazon Machine Image
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Data source for Ubuntu 22.04 LTS Amazon Machine Image
+data "aws_ami" "ubuntu_22_04" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
