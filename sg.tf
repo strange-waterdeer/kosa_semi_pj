63718bd673ff1d9e8ac9500ec2b983712266fb62
kihwan <emotion1998k@gmail.com>
2024-06-05 11:50:09 +0900

    security group update

--- sg.tf
+++ sg.tf
@@ -2,15 +2,31 @@
 resource "aws_security_group" "sg_web" {
   vpc_id = aws_vpc.vpc.id
 
-  egress = {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -23,10 +39,24 @@ resource "aws_security_group" "sg_web" {
 resource "aws_security_group" "sg_db" {
   vpc_id = aws_vpc.vpc.id
 
-  egress = {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
